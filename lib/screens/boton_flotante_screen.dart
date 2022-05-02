fc6eac6895ddd82562148d332ef03a5a59c6b6af
FlutLab.io <>
2022-05-02 22:38:05 +0000

    Autosave

--- lib/screens/boton_flotante_screen.dart
+++ lib/screens/boton_flotante_screen.dart
@@ -1,47 +1,22 @@
 import 'package:flutter/material.dart';
 
 class BotonFlotante extends StatefulWidget {
-  _BotonFlotante createState()=>_BotonFlotante();
-  }
+  _BotonFlotante createState() => _BotonFlotante();
+}
 
-  class _BotonFlotante extends State<BotonFlotante>{
-    String txt="";
+class _BotonFlotante extends State<BotonFlotante> {
+  String txt = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text("Boton flotante")
-          )
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(txt)
-              ]
-            )
-          ),
-          floatingActionButton: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              FloatingActionButton(
-                onPressed:()=> setState(()=> txt="Hola Mundo"),
-                child:Icon(
-                  Icons.visibility,
-                  size: 30
-                )
-              ),
-              FloatingActionButton(
-                  onPressed:()=> setState(()=> txt=""),
-                  child:Icon(
-                  Icons.visibility_off,
-                  size: 30
-                  )
-                 ),
-            ]
-    );
+        appBar: AppBar(title: Center(child: Text("Boton flotante"))),
+        body: Center(
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(txt)
+        ])),
+        floatingActionButton: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          FloatingActionButton(onPressed: () => setState(() => txt = "Hola Mundo"), child: Icon(Icons.visibility, size: 30)),
+          FloatingActionButton(onPressed: () => setState(() => txt = ""), child: Icon(Icons.visibility_off, size: 30)),
+        ]));
   }
-  }
-
-
+}
